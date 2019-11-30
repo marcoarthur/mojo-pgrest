2612a5504065c31274762d412895459ad120c42d
Marco Arthur <arthurpbs@gmail.com>
2019-11-30 14:03:26 -0300

    Yet removing

--- lib/UbaJobs.pm
+++ lib/UbaJobs.pm
@@ -1,7 +1,6 @@
 package UbaJobs;
 use Mojo::Base 'Mojolicious', -signatures;
 use Mojo::JSON qw( encode_json decode_json );
-use Mojo::File;
 use DDP;
 
 has pg_rest => sub { 'http://localhost:4000' };
@@ -29,14 +28,8 @@ sub _load_plugins ($self) {
 # Load OpenAPI from postgREST, setting routes
 sub _load_openapi ( $self, $url ) {
 
-    # get schema from pgREST, place x-mojo-to refs
     my $json = $self->ua->get($url)->result->json or die "Can't get schema";
     my $p = $self->plugin( 'OpenAPI' => { url => $json } );
-
-    # save the API spec in a file for debugging reasons
-    my $fh = Mojo::File->new("./spec.json")->open(">");
-    print $fh encode_json($json);
-    $fh->close;
 
     # TODO: create a format rules for these fields
     ## no critic Subroutines::ProhibitExplicitReturnUndef
